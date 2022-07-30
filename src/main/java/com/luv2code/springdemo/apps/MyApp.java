f2f2e7cc293a7a80570006ea3359a6f4f150853b
Kamil Barcik <kamilbar89@gmail.com>
2022-07-30 17:23:12 +0200

    Added Spring container with .xml config file

--- src/main/java/com/luv2code/springdemo/apps/MyApp.java
+++ src/main/java/com/luv2code/springdemo/apps/MyApp.java
@@ -1,11 +1,14 @@
-package com.luv2code.springdemo;
+package com.luv2code.springdemo.apps;
+
+import com.luv2code.springdemo.Coach;
+import com.luv2code.springdemo.TrackCoach;
 
 public class MyApp {
 
     public static void main(String[] args) {
 
         // create the object
-        Coach theCoach = new BaseballCoach();
+        Coach theCoach = new TrackCoach();
 
         // use the object
         System.out.println(theCoach.getDailyWorkout());
